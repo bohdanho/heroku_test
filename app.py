394115b142aeb60f36ba97f96b5091c9b9d7c316
bohdanho <66442661+bohdanho@users.noreply.github.com>
2021-01-09 16:12:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request
 import logging
-from telegram import Update, Bot
-from telegram.ext import Updater
+from telegram import Update, Bot, ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from threading import Thread
 import os
 
 app = Flask(__name__)
@@ -10,6 +11,59 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+
+
+def start(update, context):
+    update.message.reply_text('Тебе вітає СБ')
+    help(update, context)
+
+
+# /about
+def about(update, context):
+    update.message.reply_text('Якщо треба допомога, то пиши мені в пп')
+
+
+# /help
+def help(update, context):
+    update.message.reply_text('Вибирай потрібну команду:\n'
+                              '/help - Список доступних команд\n'
+                              '/about - Дізнатись більше про СБ\n'
+                              '/spiv - Пошук пісень')
+
+
+# /spiv
+def spiv(update, context):
+    reply_keyboard = [['Пошук пісні', 'Категорії'],
+                      ['В головне меню']]
+    update.message.reply_text("Вибери метод пошуку: ", reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+
+def echo(update, context):
+    print(update)
+    update.message.reply_text("Greeting, " + update['message']['chat']['first_name'])
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    # Boot up the bot with const TELEGRAM_TOKEN
+    # Different commands
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("about", about))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("spiv", spiv))
+    # On message
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    thread = Thread(target=dp.start, name='dp')
+    thread.start()
+
 
 @app.route(f"/{TELEGRAM_TOKEN}", methods=['GET', 'POST'])
 def webhook():
@@ -22,8 +76,6 @@ def webhook():
         return "OK"
     else:
         return "BAD"
-
-
 
 
 if __name__ == "__main__":
