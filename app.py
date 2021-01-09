7d68154e7c5ea05592b09c58b4d25313597bd279
bohdanho <66442661+bohdanho@users.noreply.github.com>
2021-01-09 15:51:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 def webhook():
     if request.method == "POST":
         # retrieve the message in JSON and then transform it to Telegram object
-        update = Update.de_json(request.get_json(force=True))
+        update = Update.de_json(request.get_json(force=True), bot=updater.bot)
         print(update)
         logger.info("Update received! " + update.message.text)
         dp.process_update(update)
@@ -23,15 +23,15 @@ def webhook():
     else:
         return "BAD"
 
-    
+
 
 
 if __name__ == "__main__":
     PORT = int(os.environ.get('PORT', '8443'))
     updater = Updater(TELEGRAM_TOKEN)
-    #updater.start_webhook(listen="0.0.0.0",
-    #                      port=PORT,
-    #                      url_path=TELEGRAM_TOKEN)
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=TELEGRAM_TOKEN)
     updater.bot.setWebhook(f"https://testflasksbbot.herokuapp.com/{TELEGRAM_TOKEN}")
     dp = updater.dispatcher
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)), threaded=True)
